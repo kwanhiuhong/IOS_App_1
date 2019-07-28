d5e8a57b77fa35db124e65a8230068a2baad37e2
Kwan Hiu Hong <hiuhongkwan@gmail.com>
2019-07-28 14:50:50 +0800

    Updated the xcode

---
+++ hustle-mode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hustle-mode
+//
+//  Created by KHH on 28/7/2019.
+//  Copyright © 2019 Kwan Hiu Hong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
