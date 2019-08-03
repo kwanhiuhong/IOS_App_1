ad51d74d7e725dd3d09870832e91aaf11e2b7aae
Kwan Hiu Hong <hiuhongkwan@gmail.com>
2019-08-03 16:58:50 +0800

    Included App icon and the launchPage for the app

--- hustle-mode/ViewController.swift
+++ hustle-mode/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -14,18 +15,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var powerBtn: UIButton!
     @IBOutlet weak var cloudHolder: UIView!
     @IBOutlet weak var rocket: UIImageView!
-    @IBOutlet weak var hustleLbl: UILabel!
-    @IBOutlet weak var onLbl: UILabel!
+    @IBOutlet weak var HustleOnStack: UIStackView!
+    
+    var player: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = Bundle.main.path(forResource: "hustle-on", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+        } catch let error as NSError {
+            print(error.description)
+        }
     }
 
     @IBAction func powerBtnPressed(_ sender: Any) {
         cloudHolder.isHidden = false
         darkBlueBG.isHidden = true
         powerBtn.isHidden = true
+        
+        player.play()
+        
+        UIView.animate(withDuration: 2.3, animations: {
+                self.rocket.frame = CGRect(x: 0, y: 20, width: 375, height: 328)
+            }) { (finished) in
+                self.HustleOnStack.isHidden = false
+            }
     }
     
 }
